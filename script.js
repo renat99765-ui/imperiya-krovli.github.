8f8954a9a4e0544538fb3a28088b85f77c6b1936
renat99765-ui <renat.99765@gmail.com>
2025-10-18 23:14:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Плавная прокрутка к якорям + коррекция под шапку
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    if (targetElement) {
+      const headerOffset = 70;
+      const elementPosition = targetElement.getBoundingClientRect().top;
+      const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth"
+      });
+    }
+  });
+});
